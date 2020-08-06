61d5ad04311a00a199ba743c94e7ece0d526c8c1
Samuel Lampa <samuel.lampa@rilnet.com>
2020-08-06 09:34:02 +0200

    Add test for whole workflow

--- ey.py
+++ ey.py
@@ -4,7 +4,7 @@ import re
 def shell(cmd):
     cmd = __replace_ports(cmd)
     out = sp.run(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, check=True, shell=True)
-    return TaskInfo()
+    return TaskInfo(out)
 
 def __replace_ports(cmd):
     ms = re.findall(r'(\[o\:([a-z]*):([a-z\.]*)\])', cmd, flags=re.S)
@@ -13,5 +13,11 @@ def __replace_ports(cmd):
     return cmd
 
 class TaskInfo:
+    def __init__(self, cmdout):
+        self.args = cmdout.args
+        self.returncode = cmdout.returncode
+        self.stdout = cmdout.stdout
+        self.stderr = cmdout.stderr
+
     def out(self, portname):
         return 'untitled.txt'
--- test_ey.py
+++ test_ey.py
@@ -1,19 +1,19 @@
 import ey
+import os.path
 from pytest import fail
 
 def test_replace_ports():
     for input, expected in [
             (
-                'wget -O [o:fasta:chry.fa]',
-                'wget -O chry.fa'
+                'wget -O [o:fasta:chrmt.fa]',
+                'wget -O chrmt.fa'
             )
         ]:
         output = ey.__replace_ports(input)
         if output != expected:
             fail('output not as expected')
 
-#def test_ey():
-#    fasta = ey.shell(cmd)
-#    with open(fasta.out('fasta')) as fafile:
-#        for line in fafile:
-#            print(line)
+def test_ey():
+    fasta = ey.shell('wget -O [o:gz:chrmt.fa.gz] ftp://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz')
+    if not os.path.isfile('chrmt.fa.gz'):
+        fail('download did not succeed')
