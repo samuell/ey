0ee1469040f8c9acd9159d581773c402ad012293
Samuel Lampa <samuel.lampa@rilnet.com>
2020-08-06 09:45:57 +0200

    Refactor some methods into Task class

--- ey.py
+++ ey.py
@@ -2,22 +2,39 @@ import subprocess as sp
 import re
 
 def shell(cmd):
-    cmd = __replace_ports(cmd)
+    task = Task()
+    task._init_from_command(cmd)
+
     out = sp.run(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, check=True, shell=True)
-    return TaskInfo(out)
+    task._add_cmd_results(out)
 
-def __replace_ports(cmd):
-    ms = re.findall(r'(\[o\:([a-z]*):([a-z\.]*)\])', cmd, flags=re.S)
-    for m in ms:
-        cmd = cmd.replace(m[0], m[2])
-    return cmd
+    return task
 
-class TaskInfo:
-    def __init__(self, cmdout):
+
+class Task:
+    def __init__(self):
+        pass
+
+    # ------------------------------------------------
+    # Public methods
+    # ------------------------------------------------
+    def out(self, portname):
+        return 'untitled.txt'
+
+    # ------------------------------------------------
+    # Internal methods
+    # ------------------------------------------------
+    def _add_cmd_results(self, cmdout):
         self.args = cmdout.args
         self.returncode = cmdout.returncode
         self.stdout = cmdout.stdout
         self.stderr = cmdout.stderr
 
-    def out(self, portname):
-        return 'untitled.txt'
+    def _init_from_command(self, cmd):
+        cmd = self._replace_ports(cmd)
+
+    def _replace_ports(self, cmd):
+        ms = re.findall(r'(\[o\:([a-z]*):([a-z\.]*)\])', cmd, flags=re.S)
+        for m in ms:
+            cmd = cmd.replace(m[0], m[2])
+        return cmd
--- test_ey.py
+++ test_ey.py
@@ -3,17 +3,20 @@ import os.path
 from pytest import fail
 
 def test_replace_ports():
+    task = ey.Task()
     for input, expected in [
             (
                 'wget -O [o:fasta:chrmt.fa]',
                 'wget -O chrmt.fa'
             )
         ]:
-        output = ey.__replace_ports(input)
+        output = task._replace_ports(input)
         if output != expected:
             fail('output not as expected')
 
 def test_ey():
-    fasta = ey.shell('wget -O [o:gz:chrmt.fa.gz] ftp://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz')
+    gz = ey.shell('wget -O [o:gz:chrmt.fa.gz] ftp://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz')
     if not os.path.isfile('chrmt.fa.gz'):
         fail('download did not succeed')
+
+
